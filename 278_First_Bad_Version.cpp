b503402cbed3652185ae2f03f77d83c81374caaf
criss <criss.lin@hotmail.com>
2017-07-17 20:24:14 +0800

    278 bi-search

---
+++ 278_First_Bad_Version.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+//bool isBadVersion(int version){
+//  if(version>=5){
+//    return true;
+//  }
+//  else{
+//    return false;
+//  }
+//}
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+      int l=1, r=n;
+      while(l<r){
+        int mid=l+(r-l)/2;
+        if(isBadVersion(mid)){
+          r=mid;
+        }
+        else{
+          l=mid+1;
+        }
+      }
+      return l;
+    }
+};
+
+int main(){
+  cout << Solution().firstBadVersion(5) << endl;
+  return 0;
+}
